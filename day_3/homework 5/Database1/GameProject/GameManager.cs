df8829e13c1c13a957041f6ea937e55644cf4142
cengiz <cengizucak@outlook.com>
2022-10-13 23:20:53 +0200

    Add files via upload

---
+++ day_3/homework 5/Database1/GameProject/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject
+{
+
+    //microservice
+    class GameManager : IGameService
+    {
+        public void Add(Gamer gamer)
+        {
+            Console.WriteLine("Kayit oldu");
+            throw new NotImplementedException();
+        }
+        public void Delete(Gamer gamer)
+        {
+            Console.WriteLine("Silindi");
+            throw new NotImplementedException();
+        }
+        public void Update(Gamer gamer)
+        {
+            Console.WriteLine("Guncellendi");
+            throw new NotImplementedException();
+        }
+    }
+}
